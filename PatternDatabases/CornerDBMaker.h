b797782ee51af3aad452827fca5b18fce5b235dd
VisheshGupta2004 <visheshg2004@gmail.com>
2025-08-16 12:43:21 +0530

    commit

---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,26 @@
+//
+//
+// Created by Vishesh Gupta in June-2025.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#define RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#include "CornerPatternDatabase.h"
+#include "../Model/RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
